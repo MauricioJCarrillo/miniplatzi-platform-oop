40cbfd53960a6b3dab0e17dba81ee231b0cccca5
Mauricio J Carrillo - Laptop <mauricio.jcarrillo@gmail.com>
2021-10-20 09:52:29 -0500

    6to commit: Unificación de los archivos

--- course.js
+++
@@ -1,9 +0,0 @@
-export default class Course {
-    constructor({
-        name,
-        classes = [],
-    }){
-        this.name = name;
-        this.classes = classes;        
-    }
-}
--- main.js
+++ main.js
@@ -1,26 +1,104 @@
-class Student {
+// Creación de la Clase - Prototipo Comment
+class Comment {
     constructor({
-        name, 
-        email,
-        username,
-        twitter = undefined,
-        instagram = undefined,
-        facebook = undefined,
-        cursosAprobados = [],
-        learningPaths = [],
-    }) {
-        this.name = name;
-        this.email = email;
-        this.username = username;
-        this.socialMedia = {
-            twitter,
-            instagram,
-            facebook,
-        };
-        this.cursosAprobados = cursosAprobados;
-        this.learningPaths = learningPaths;
+        studentName,
+        studentRole = "Estudiante",
+        content,
+    }){
+        this.studentName = studentName;
+        this.studentRole = studentRole;
+        this.content = content;
+        this.likes = 0;
+    }
+    publicar(){
+        console.log(this.studentName + " (" + this.studentRole + ")");
+        console.log(this.likes + " likes");
+        console.log(this.content);
     }
 }
+// Creación de la Clase - Prototipo PlatziClass
+class PlatziClass {
+    constructor({
+        name,
+        videoID,        
+    }){
+        this.name = name;
+        this.videoID = videoID;
+    }
+    reproducir() {
+        videoPlay(this.videoID);
+    }
+    pausar() {
+        videoStop(this.videoID);
+    }
+}
+function videoPlay(id) {
+    const urlSecreta = "https://platziultrasecreto.com";
+    console.log("Se está reproduciendo desde la url " + urlSecreta);
+}
+function videoStop(id) {
+    const urlSecreta = "https://platziultrasecreto.com";
+    console.log("Pausamos la url " + urlSecreta);
+}
+// Nuevas instancias de la clase Class
+const clase67 = new PlatziClass({
+    name: "JavaScript: Orientado a prototipos",
+    videoID: "555555",
+});
+// Creación de la Clase - Prototipo Course
+class Course {
+    constructor({
+        name,
+        classes = [],
+        isFree = false,
+        lang = "spanish",
+    }){
+        this._name = name;
+        this.classes = classes;        
+        this.isFree = isFree;        
+        this.lang = lang;        
+    }
+    get name() {
+        return this._name;
+    }
+    set name(nuevoNombre) {
+        if(nuevoNombre === "Curso Malito de programación Básica"){
+            console.error("Wey... no");
+        }else{
+            this._name = nuevoNombre;            
+        }
+        this._name = nuevoNombre;
+    }
+}
+// Nuevas instancias de la clase Course
+const cursoProgBasica = new Course({
+    name: "Curso Gratis de Programación básica",
+    classes: [
+        "1. Introducción",
+        "2. Desarrollo básico",
+        "3. Desarrollo avanzado",
+    ],
+    isFree: true,
+    lang: "english",
+});
+const cursoDefinitvoHTML = new Course({
+    name: "Curso Definitivo de HTML y CSS",
+    classes: [
+        "1. Introducción",
+        "2. Definitivo básico",
+        "3. Definitivo avanzado",
+    ],
+});
+const cursoPracticoHTML = new Course({
+    name: "Curso Práctico de HTML y CSS",
+    classes: [
+        "1. Introducción",
+        "2. Práctico básico",
+        "3. Práctico avanzado",
+    ],
+    lang: "english",
+});
+// Creación de la Clase - Prototipo LearningPaths
 class LearningPaths {
     constructor({
         name,
@@ -36,36 +114,6 @@ class LearningPaths {
         this.courses.pop(nuevoCursoEliminado);           
     }
 }
-class Course {
-    constructor({
-        name,
-        classes = [],
-    }){
-        this.name = name;
-        this.classes = classes;        
-    }
-}
-// Nuevas instancias de la clase Student, no importa el orden de los parametros (RORO).
-const juan = new Student({
-    name: "JuanDC",
-    username: "juandc",
-    email: "juanito@juanito.com", 
-    twitter: "fjuandc", 
-    learningPaths: [
-        escuelaDesarrolloWeb,
-        escuelaVideoJuegos,
-    ],
-});
-const miguelito = new Student({
-    name: "Miguelito",
-    username: "miguelitofeliz",
-    email: "miguelito@miguelito.com", 
-    instagram: "miguelito_feliz",
-    learningPaths: [
-        escuelaDesarrolloWeb,
-        escuelaDataScientist,
-    ], 
-})
 // Nuevas instancias de la clase LearningPath
 const escuelaDesarrolloWeb = new LearningPaths({
     name: "Escuela de Desarrollo Web",
@@ -91,28 +139,110 @@ const escuelaVideoJuegos = new LearningPaths({
         "Curso de Unity 3D",
     ],
 });
-// Nuevas instancias de la clase Course
-const cursoProgBasica = new Course({
-    name: "Curso Gratis de Programación básica",
-    classes: [
-        "1. Introducción",
-        "2. Desarrollo básico",
-        "3. Desarrollo avanzado",
+// Creación de la Clase - Prototipo Student
+class Student {
+    constructor({
+        name, 
+        email,
+        username,
+        twitter = undefined,
+        instagram = undefined,
+        facebook = undefined,
+        cursosAprobados = [],
+        learningPaths = [],
+    }) {
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.socialMedia = {
+            twitter,
+            instagram,
+            facebook,
+        };
+        this.cursosAprobados = cursosAprobados;
+        this.learningPaths = learningPaths;
+    }
+    publicarComentario(contenidoComentario) {
+        const comentario = new Comment({
+            content: contenidoComentario,
+            studentName: this.name,
+        });
+        comentario.publicar();
+    }
+}
+//Herencia de JS: Nuevas clases que heredan los atributos y metodos de la clase Student
+class FreeStudent extends Student {
+    constructor(props) {
+        super(props);
+    }
+    aprobarCurso(nuevoCurso) {
+        if(nuevoCurso.isFree) {
+            this.cursosAprobados.push(nuevoCurso);
+        }else {
+            console.warn("Lo sentimos, " + this.name + ", solo puedes tomar cursos gratuitos");
+        }
+    }
+}
+class BasicStudent extends Student {
+    constructor(props) {
+        super(props);
+    }
+    aprobarCurso(nuevoCurso) {
+        if(nuevoCurso.lang !== "english") {
+            this.cursosAprobados.push(nuevoCurso);
+        }else {
+            console.warn("Lo sentimos, " + this.name + ", no puedes tomar cursos en inglés");
+        }
+    }
+}
+class ExpertStudent extends Student {
+    constructor(props) {
+        super(props);
+    }
+    aprobarCurso(nuevoCurso) {
+        this.cursosAprobados.push(nuevoCurso);
+    }
+}
+class TeacherStudent extends Student {
+    constructor(props) {
+        super(props);
+    }
+    aprobarCurso(nuevoCurso) {
+        this.cursosAprobados.push(nuevoCurso);
+    }
+    publicarComentario(contenidoComentario) {
+        const comentario = new Comment({
+            content: contenidoComentario,
+            studentName: this.name,
+            studentRole: "Profesor", 
+        });
+        comentario.publicar();
+    }
+}
+// Nuevas instancias de la clase Student, no importa el orden de los parametros (RORO).
+const juan = new FreeStudent({
+    name: "JuanDC",
+    username: "juandc",
+    email: "juanito@juanito.com", 
+    twitter: "fjuandc", 
+    learningPaths: [
+        escuelaDesarrolloWeb,
+        escuelaVideoJuegos,
     ],
 });
-const cursoDefinitvoHTML = new Course({
-    name: "Curso Definitivo de HTML y CSS",
-    classes: [
-        "1. Introducción",
-        "2. Definitivo básico",
-        "3. Definitivo avanzado",
-    ],
-});
-const cursoPracticoHTML = new Course({
-    name: "Curso Práctico de HTML y CSS",
-    classes: [
-        "1. Introducción",
-        "2. Práctico básico",
-        "3. Práctico avanzado",
-    ],
-});
+const miguelito = new BasicStudent({
+    name: "Miguelito",
+    username: "miguelitofeliz",
+    email: "miguelito@miguelito.com", 
+    instagram: "miguelito_feliz",
+    learningPaths: [
+        escuelaDesarrolloWeb,
+        escuelaDataScientist,
+    ], 
+}) 
+const freddy = new TeacherStudent({
+    name: "Freddy Vega",
+    username: "Freddier",
+    email: "freddyvega@platzi.com", 
+    instagram: "freddiervega", 
+}) 
